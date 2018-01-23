cfc6efbcbaa20111ca135e71f552d2ba50e410b1
Unknown <g.sunzc@gmail.com>
2018-01-23 17:12:35 +0800

    realm

--- funs-admin/src/main/java/org/sun/admin/shiro/AdminShiroRealm.java
+++ funs-admin/src/main/java/org/sun/admin/shiro/AdminShiroRealm.java
@@ -41,11 +41,7 @@ public class AdminShiroRealm extends AuthorizingRealm {
 	private RolePermissionService rolePermissionService;
 
 	/**
-	 * 认证信息 Authentication 是用来验证用户身份
-	 * 
-	 * @param token
-	 * @return
-	 * @throws AuthenticationException
+	 * 认证
 	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
@@ -65,15 +61,7 @@ public class AdminShiroRealm extends AuthorizingRealm {
 	}
 
 	/**
-	 * 此方法调用 hasRole,hasPermission的时候才会进行回调.
-	 *
-	 * 权限信息.(授权): 1、如果用户正常退出，缓存自动清空； 2、如果用户非正常退出，缓存自动清空；
-	 * 3、如果我们修改了用户的权限，而用户不退出系统，修改的权限无法立即生效。 （需要手动编程进行实现；放在service进行调用）
-	 * 在权限修改后调用realm中的方法，realm已经由spring管理，所以从spring中获取realm实例， 调用clearCached方法；
-	 * :Authorization 是授权访问控制，用于对用户进行的操作授权，证明该用户是否允许进行当前操作，如访问某个链接，某个资源文件等。
-	 * 
-	 * @param principals
-	 * @return
+	 * 授权
 	 */
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) throws AuthenticationException {
